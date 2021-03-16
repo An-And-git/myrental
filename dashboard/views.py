cf83838585681c1127d32d8133ad1cb1ce6a906d
Anand <anand.s2126@gmail.com>
2021-03-16 18:21:53 +0530

    from local

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,3 +5,9 @@ from .models import Dashboard
 def home(request):
     dashboard = Dashboard.objects
     return render(request, 'home/dashboard.html', {'dashboard':dashboard})
+
+def test(request):
+    context = {
+        "data": [1, 2, 3, 4, 5, 6, 7, 8],
+    }
+    return render(request, 'home/dashboard.html', {'dashboard':dashboard})
